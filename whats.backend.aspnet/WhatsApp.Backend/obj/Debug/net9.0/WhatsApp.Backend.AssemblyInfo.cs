97820a3469bfb2f4d31a2a79db5b698f5c4a04d5
engfeda-ui <eng.feda@gmail.com>
2025-10-31 04:57:56 +0200

    fix: correct typo in knowledge base service

--- whats.backend.aspnet/WhatsApp.Backend/obj/Debug/net9.0/WhatsApp.Backend.AssemblyInfo.cs
+++ whats.backend.aspnet/WhatsApp.Backend/obj/Debug/net9.0/WhatsApp.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhatsApp.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b15f8db71895d7d2ea772afc1effa0b366b51c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95aceaa5bac52a5a13a835a1eee9c5e09f244bd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhatsApp.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhatsApp.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
